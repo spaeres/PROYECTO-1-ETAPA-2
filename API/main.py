4592f8440cb9808ca733e07573dd0bb7b5e21310
Santiago Paeres <s.paeres10@uniandes.edu.co>
2023-11-10 21:01:18 -0500

    Arreglo API

--- API/main.py
+++ API/main.py
@@ -1,13 +1,19 @@
 from fastapi.responses import FileResponse
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import joblib
 import uvicorn
 from typing import Union
 
-from PredicitonModel import Model
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -21,43 +27,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 
 @app.post("/predict")
-async def upload_file(file: UploadFile):
-    # Verificar si el archivo subido es un archivo CSV
-    if not file.filename.endswith(".csv"):
-        return {"error": "Solo se permiten archivos CSV"}
-
-    try:
-        # Leer el archivo CSV con Pandas
-        df_predict = pd.read_csv(file.file, sep=';')
-        print(df_predict)
-        df_predict = df_predict['Textos_espanol']
-
-        print(df_predict.shape[0])
-
-        filename_model = "assets/tfidf_model.joblib"
-        filename_transform = "assets/tfidf_transform.pkl"
-
-        # Carga el modelo desde el archivo
-        tfidf_model = joblib.load(filename_model)
-        tfidf = joblib.load(filename_transform)
-
-        # Prediccion:
-        tfidf.transform(df_predict)
-        y_test_search_predict = tfidf_model.predict(df_predict)
-
-        print('res:', y_test_search_predict)
-
-        df_predict['sdg'] = y_test_search_predict[0:]
-
-        # Convertir el DataFrame a JSON
-        df_json = df_predict.to_json()
-
-        return df_json
-    except Exception as e:
-        return {"error": e.__str__()}
-
-
-@app.post("/predict-otro")
 async def upload_file(file: UploadFile = File(...)):
     # Verificar si el archivo subido es un archivo CSV
     if not file.filename.endswith(".csv"):
@@ -66,10 +35,7 @@ async def upload_file(file: UploadFile = File(...)):
     try:
         # Leer el archivo CSV con Pandas
         df_predict = pd.read_csv(file.file, sep=';')
-        print(df_predict)
         df_predict = df_predict['Textos_espanol']
-
-        print(df_predict.shape[0])
 
         filename_model = "assets/tfidf_model.joblib"
         filename_transform = "assets/tfidf_transform.pkl"
@@ -82,16 +48,19 @@ async def upload_file(file: UploadFile = File(...)):
         tfidf.transform(df_predict)
         y_test_search_predict = tfidf_model.predict(df_predict)
 
-        print('res:', y_test_search_predict)
+        df_final = pd.DataFrame(df_predict)
 
-        df_predict['sdg'] = y_test_search_predict[0:]
+        print(df_final)
+
+        # Insertar la nueva columna en la posición deseada
+        df_final['sdg'] = y_test_search_predict[0:]
 
         # Guardar DataFrame como un archivo CSV
         output_filename = "output_predictions.csv"
-        df_predict.to_csv(output_filename, index=False)
+        df_final.to_csv(output_filename)
 
         # Devolver el archivo como respuesta
-        return FileResponse(output_filename, media_type="text/csv", filename="output_predictions.csv")
+        return FileResponse(output_filename, filename="output_predictions.csv")
     except Exception as e:
         return {"error": e.__str__()}
 
